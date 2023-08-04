39484caf8adf47abe6ad451e1c1ae92d176e30c2
dandan-eg <94542005+dandan-eg@users.noreply.github.com>
2023-08-04 15:17:29 +0200

    feat(sout) : add print

--- src/main/java/fr/dawan/demoJenkinsMvc/DemoJenkinsMvcApplication.java
+++ src/main/java/fr/dawan/demoJenkinsMvc/DemoJenkinsMvcApplication.java
@@ -8,6 +8,7 @@ public class DemoJenkinsMvcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoJenkinsMvcApplication.class, args);
+		System.out.println("whoosh");
 	}
 
 }
